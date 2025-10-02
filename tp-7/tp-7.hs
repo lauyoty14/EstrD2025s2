8630bfbeb6b6d7a6ec2e5b291cb3f8ac225bf695
Octavio Pastorive <114613692+lauyoty14@users.noreply.github.com>
2025-10-02 17:51:26 -0300

    pequeños avanzes

--- tp-7/tp-7.hs
+++ tp-7/tp-7.hs
@@ -1,12 +1,10 @@
-import Data.IntMap (insert, split)
-import Data.Sequence (Seq(Empty))
 -- ejercicio 1 : heapsort es de costo O(n log n) siendo n la cantidad de elementos de la lista
 -- ejercicio 2
-data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
+data TreeBST a = EmptyTB | NodeTB a (TreeBST a) (TreeBST a)
 
-belongsBST :: Ord a => a -> Tree a -> Bool
-belongsBST _ EmptyT = False
-belongsBST x (NodeT y ti td) =  if(x == y)
+belongsBST :: Ord a => a -> TreeBST a -> Bool
+belongsBST _ EmptyTB = False
+belongsBST x (NodeTB y ti td) =  if(x == y)
                                 then True
                                 else if (x < y)
                                     then belongsBST x ti
@@ -14,55 +12,83 @@ belongsBST x (NodeT y ti td) =  if(x == y)
 -- Propósito: dado un BST dice si el elemento pertenece o no al árbol.
 -- Costo: O(log N)
 
-insertBST :: Ord a => a -> Tree a -> Tree a
-insertBST x EmptyT = NodeT x EmptyT EmptyT
+insertBST :: Ord a => a -> TreeBST a -> TreeBST a
+insertBST x EmptyTB = NodeT x EmptyTB EmptyTB
 insertBST x (NodeT y ti td) = if (x == y)
-                              then NodeT y ti td 
+                              then NodeTB y ti td 
                               else if (x < y)
-                                then NodeT y (insertBST x ti) td 
-                                else NodeT y ti (insertBST x td)
+                                then NodeTB y (insertBST x ti) td 
+                                else NodeTB y ti (insertBST x td)
 -- Propósito: dado un BST inserta un elemento en el árbol.
 -- Costo: O(log N)
 
-deleteBST :: Ord a => a -> Tree a -> Tree a
-deleteBST x EmptyT = EmptyT
-deleteBST x (NodeT y ti td) = if (x == y)
+deleteBST :: Ord a => a -> TreeBST a -> TreeBST a
+deleteBST x EmptyTB = EmptyTB
+deleteBST x (NodeTB y ti td) = if (x == y)
                               then rearmarBST ti td 
                               else if (x < y) 
-                                then NodeT y (deleteBST x ti) td 
-                                else NodeT y ti (deleteBST x td)
+                                then NodeTB y (deleteBST x ti) td 
+                                else NodeTB y ti (deleteBST x td)
 -- Propósito: dado un BST borra un elemento en el árbol.
 -- Costo: O(log N)
 
-rearmarBST :: Ord a => Tree a -> Tree a -> Tree a
+rearmarBST :: Ord a => TreeBST a -> TreeBST a -> TreeBST a
  -- PRECOND: ambos árboles son BSTs
-rearmarBST EmptyT td = td
-rearmarBST ti td = NodeT (maxBST ti) (delMaxBST ti) td
+rearmarBST EmptyTB td = td
+rearmarBST ti td = NodeTB (maxBST ti) (delMaxBST ti) td
 
-maxBST :: Tree a -> a
-maxBST (NodeT x _ EmptyT) = x -- PRECOND: no es vacío
-maxBST (NodeT _ _ td) = maxBST td
+maxBST :: TreeBST a -> a
+maxBST (NodeTB x _ EmptyTB) = x -- PRECOND: no es vacío
+maxBST (NodeTB _ _ td) = maxBST td
 
-delMaxBST :: Tree a -> Tree a
-delMaxBST (NodeT _ ti EmptyT) = ti -- PRECOND: no es vacío
-delMaxBST (NodeT x ti td) = NodeT x ti (delMaxBST td)
+delMaxBST :: TreeBST a -> TreeBST a
+delMaxBST (NodeTB _ ti EmptyTB) = ti -- PRECOND: no es vacío
+delMaxBST (NodeTB x ti td) = NodeTB x ti (delMaxBST td)
 
-splitMinBST :: Ord a => Tree a -> (a, Tree a)
-splitMinBST EmptyT = error "no hay minimo elemento"
-splitMinBST (NodeT x ti td) = (minBST ti , delMinBST ti)
+splitMinBST :: Ord a => TreeBST a -> (a, TreeBST a)
+splitMinBST EmptyTB = error "no hay minimo elemento"
+splitMinBST (NodeTB x ti td) = (minBST ti , delMinBST ti)
 -- Propósito: dado un BST devuelve un par con el mínimo elemento y el árbol sin el mismo.
 -- Costo: O(log N)
 
-minBST :: Tree a -> a
-minBST (NodeT x EmptyT _) = x -- PRECOND : el tree no es vacio
-minBST (NodeT _ ti _) = minBST ti
+minBST :: TreeBST a -> a
+minBST (NodeTB x EmptyTB _) = x -- PRECOND : el tree no es vacio
+minBST (NodeTB _ ti _) = minBST ti
 
-delMinBST :: Tree a -> Tree a
-delMinBST (NodeT _ EmptyT td) = td -- PRECOND: no es vacío
-delMinBST (NodeT x ti td) = NodeT x (delMinBST ti) td
+delMinBST :: TreeBST a -> TreeBST a
+delMinBST (NodeTB _ EmptyTB td) = td -- PRECOND: no es vacío
+delMinBST (NodeTB x ti td) = NodeTB x (delMinBST ti) td
 
-splitMaxBST :: Ord a => Tree a -> (a, Tree a)
-splitMaxBST EmptyT = error "no hay maximo elemento"
-splitMaxBST (NodeT x ti td) = (maxBST td, delMaxBST td)
+splitMaxBST :: Ord a => TreeBST a -> (a, TreeBST a)
+splitMaxBST EmptyTB = error "no hay maximo elemento"
+splitMaxBST (NodeTB x ti td) = (maxBST td, delMaxBST td)
 -- Propósito: dado un BST devuelve un par con el máximo elemento y el árbol sin el mismo.
 -- Costo: O(log N)
+ 
+esBST :: TreeBST a -> Bool
+esBST EmptyTB = True
+esBST (NodeTB x ti td) = esMayorAlArbol x ti && esMenorAlArbol x td
+-- Propósito: indica si el árbol cumple con los invariantes de BST.
+-- Costo: O(N 2)
+
+esMayorAlArbol :: Ord a -> a -> TreeBST a -> Bool
+esMayorAlArbol x EmptyTB = True 
+esMayorAlArbol x (NodeTB y ti td) = x > y && esMayorAlArbol x ti && esMayorAlArbol x td
+
+esMenorAlArbol :: Ord a -> a -> TreeBST a -> Bool
+esMenorAlArbol x EmptyTB = True
+esMenorAlArbol x (NodeTB y ti td) = x < y && esMenorAlArbol x ti && esMenorAlArbol x td
+
+elMaximoMenorA :: Ord a => a -> TreeBST a -> Maybe a
+elMaximoMenorA x treeBST = 
+                case maximoMenor x treeBST of
+                    Just v -> v 
+                    Nothing -> error "no hay un maximo menor"   
+-- Propósito: dado un BST y un elemento, devuelve el máximo elemento que sea menor al
+-- elemento dado.
+-- Costo: O(log N )
+
+maximoMenor :: Ord a => a -> TreeBST a -> Maybe a
+maximoMenor x (NodeTB y ti td) = if y < x && y > maximoMenor x ti 
+                                 then Just y
+                                 else maximoMenor x ti 
